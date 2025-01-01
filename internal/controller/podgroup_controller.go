e9c4e27133b181a0aa48ba24c59a11d638cd3fed
haotiancheng <919263118@qq.com>
2025-01-01 18:22:02 +0800

    add podgroup reconcile logic

--- internal/controller/podgroup_controller.go
+++ internal/controller/podgroup_controller.go
@@ -18,13 +18,18 @@ package controller
 
 import (
 	"context"
+	"fmt"
+	"time"
 
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	schedulingv1 "github.com/HtcOrange/gang-scheduler-api/api/v1"
+	v1 "github.com/HtcOrange/gang-scheduler-api/api/v1"
 )
 
 // PodGroupReconciler reconciles a PodGroup object
@@ -47,11 +52,71 @@ type PodGroupReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.19.1/pkg/reconcile
 func (r *PodGroupReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	logger := log.FromContext(ctx)
 
-	// TODO(user): your logic here
+	// get podgroup
+	var podGroup schedulingv1.PodGroup
+	if err := r.Get(ctx, req.NamespacedName, &podGroup); err != nil {
+		if errors.IsNotFound(err) {
+			// podgroup has been deleted
+			return ctrl.Result{}, err
+		}
+	}
 
-	return ctrl.Result{}, nil
+	// get podlist in one podgroup
+	var pods corev1.PodList
+	if err := r.List(ctx, &pods, client.InNamespace(podGroup.Namespace), client.MatchingLabels{"pod-group": podGroup.Spec.GroupName}); err != nil {
+		logger.Error(err, "Failed to get pod list")
+		return ctrl.Result{}, err
+	}
+	podCount := len(pods.Items)
+	logger.Info("PodGroup status", "GroupName", podGroup.Spec.GroupName, "PodCount", podCount)
+
+	scheduledPodNum, podMetaList := getPodsScheduledNumAndMetaList(pods)
+	podGroup.Status.PodMetaList = podMetaList
+	podGroup.Status.Message = fmt.Sprintf("%d pods has been created, %d pods has been scheduled", podCount, scheduledPodNum)
+
+	// sync PodGroup status
+	if podCount >= podGroup.Spec.MinNum {
+		if scheduledPodNum < podCount {
+			// some of pods are still pending
+			podGroup.Status.Phase = "CreatedReadyAndScheduling"
+		} else {
+			// all of the pods are scheduled
+			podGroup.Status.Phase = "Ready"
+			podGroup.Status.Message = fmt.Sprintf("PodGroup %s is Ready", podGroup.Spec.GroupName)
+		}
+	} else {
+		// count of pods created is less than min num
+		podGroup.Status.Phase = "Waiting"
+	}
+
+	// update PodGroup status
+	if err := r.Status().Update(ctx, &podGroup); err != nil {
+		logger.Error(err, "Update PodGroup status failed", podGroup.Spec.GroupName)
+		return ctrl.Result{}, nil
+	}
+
+	return ctrl.Result{RequeueAfter: 3 * time.Second}, nil
+}
+
+func getPodsScheduledNumAndMetaList(podList corev1.PodList) (int, []v1.PodMeta) {
+	var scheduledCount int
+	var podMetaList []v1.PodMeta
+	for _, pod := range podList.Items {
+		if pod.Spec.NodeName != "" {
+			// pod's spec nodeName has been added
+			scheduledCount++
+		}
+		meta := v1.PodMeta{
+			Name:   pod.Name,
+			IP:     pod.Status.PodIP,
+			HostIP: pod.Status.HostIP,
+			Phase:  string(pod.Status.Phase),
+		}
+		podMetaList = append(podMetaList, meta)
+	}
+	return scheduledCount, podMetaList
 }
 
 // SetupWithManager sets up the controller with the Manager.
